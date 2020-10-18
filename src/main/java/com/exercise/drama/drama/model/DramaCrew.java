9967ba34895f59385cb7d6ccae031adeb1dacbbb
Sai Han <thesaihan@gmail.com>
2020-10-18 15:00:04 +0630

    get list of crews in drama

---
+++ src/main/java/com/exercise/drama/drama/model/DramaCrew.java
@@ -0,0 +1,58 @@
+package com.exercise.drama.drama.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class DramaCrew implements Serializable {
+
+    @Id
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "drama_id", nullable = false)
+    private Dramas dramas;
+    
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id", nullable = false)
+    @JsonIgnoreProperties("roles")
+    private Person person;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Dramas getDramas() {
+        return dramas;
+    }
+
+    public void setDramas(Dramas dramas) {
+        this.dramas = dramas;
+    }
+    
+}
